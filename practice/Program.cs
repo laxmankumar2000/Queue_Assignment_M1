d5979b5a33fefc8938ef9c1c05f2b0707a8a7c96
laxmankumar2000 <laxmankumarvashisth@gmail.com>
2024-06-26 15:02:19 +0530

    Add project files.

---
+++ practice/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    public class Program
+    {
+        public static Queue detailsQueue = new Queue();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("1.Add into Queue");
+            Console.WriteLine("2.Get first Details");
+            Console.WriteLine("3. remove the first deatils");
+            Console.WriteLine();
+            Console.WriteLine();
+            int choice = 0;
+
+            while (choice != 5)
+            {
+                Console.WriteLine("Enter the choice");
+                 choice = int.Parse(Console.ReadLine());
+                details d = new details();
+
+                switch (choice)
+                {
+
+                    case 1:
+                        {
+                            Console.WriteLine("Enter the name of user");
+                            string user = Console.ReadLine();
+                            string res = d.AddIntoQueue(user);
+                            Console.WriteLine(res);
+                            break;
+                        }
+                    case 2:
+                        {
+                            string ans = d.getFirstNumber();
+                            if (ans == "")
+                                Console.WriteLine("Queue is Empty");
+                            else
+                                Console.WriteLine(ans);
+                            break;
+                        }
+                    case 3:
+                        {
+                            string ans = d.getFirstPhone();
+                            if(ans == "")
+                                Console.WriteLine("Queue is Empty");
+                            else
+                                Console.WriteLine(ans);
+
+                            break;
+                        }
+                    case 4:
+                        {
+                            d.printQueue();
+                            break;
+                        }
+
+                }
+            }
+            
+        }
+    }
+}
---
+++ practice/details.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    public class details
+    {
+           
+        public string AddIntoQueue(string user)
+        {
+            //Alice-9090909090
+            
+            int count = 0;
+            for(int i = 0;i<user.Length;i++)
+            {
+                if (user[i]=='-')
+                    count++;
+            }
+            //bob9090909090
+            if (count != 1)
+            {
+                return "*";
+            }
+            //-90909090909099
+            string[] str = user.Split('-');
+            //Console.WriteLine(str.Length);
+
+            if (str[0] == "" || str[1] == "")
+            {
+                return "!";
+            }
+/*
+            foreach (string str2 in str)
+            {
+                Console.WriteLine(str2);
+}
+            
+*/
+
+            //alice-3434
+
+            if (str[1].Length != 10)
+                return "@";
+
+            Program.detailsQueue.Enqueue(user);
+
+
+            return "";
+        }
+
+        public string getFirstNumber()
+        {
+            if (Program.detailsQueue.Count != 0)
+            {
+                string res1 = "";
+                if (Program.detailsQueue.Count != 0)
+                {
+                    object res = Program.detailsQueue.Peek();
+                    res1 = (string)res;
+                }
+                return res1;
+            }
+            
+            return "";
+        }
+
+        public string getFirstPhone()
+        {
+            string[] strarr;
+            string[] strand;
+            if (Program.detailsQueue.Count != 0)
+            {
+
+                Object res = Program.detailsQueue.Peek();
+
+                string res1 = (string)res;
+
+                 strarr = res1.Split('-');
+
+                Object and = Program.detailsQueue.Dequeue();
+
+                string and1 = (string)and;
+
+                strand = and1.Split('-');
+                return strand[1];
+            }
+
+            return "";
+        }
+
+        public void printQueue()
+        {
+            //IEnumerator ie = Program.detailsQueue.GetEnumerator();
+            Console.WriteLine("COubnt is " +  Program.detailsQueue.Count);
+            foreach (var item in Program.detailsQueue)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+    }
+}
